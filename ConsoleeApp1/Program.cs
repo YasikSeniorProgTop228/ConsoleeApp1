dd7aba85ca820e2547f7de737831a52df2ad19b8
YasikSeniorProgTop228 <lugovijaroslav9@gmail.com>
2024-09-24 19:58:15 +0300

    Add project files.

---
+++ ConsoleeApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    // Метод для перевірки високосного року
+    static bool IsLeapYear(int year)
+    {
+        // Перевірка умов високосного року
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Головний метод програми
+    static void Main(string[] args)
+    {
+        // Запит у користувача року
+        Console.Write("Введіть рік: ");
+        int year;
+
+        // Перевірка на правильність введення
+        if (int.TryParse(Console.ReadLine(), out year))
+        {
+            // Перевірка, чи рік високосний
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} - це високосний рік.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} - це не високосний рік.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Будь ласка, введіть коректний рік.");
+        }
+    }
+}
